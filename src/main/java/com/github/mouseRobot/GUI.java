321a54be0bdc7e6749ee0a2d1a6dcfac2aa07705
Omar Zahed <py4object@gmail.com>
2015-05-31 20:09:33 +0300

    init

---
+++ src/main/java/com/github/mouseRobot/GUI.java
@@ -0,0 +1,8 @@
+package com.github.mouseRobot;
+
+/**
+ * Created by kozo on 5/31/15.
+ */
+
+public class GUI {
+}
---
+++ src/main/java/com/github/mouseRobot/Main.java
@@ -0,0 +1,7 @@
+package com.github.mouseRobot;
+
+/**
+ * Created by kozo on 5/31/15.
+ */
+public class Main {
+}
