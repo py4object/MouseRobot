5a3ab9631f46ef03d848e6f4bb6b9159925ac1ac
Omar Zahed <py4object@gmail.com>
2015-05-31 20:31:08 +0300

    Simple test for the GUI

--- src/test/java/GUITest.java
+++ src/test/java/GUITest.java
@@ -1,5 +1,10 @@
 /**
  * Created by kozo on 5/31/15.
  */
+import com.github.mouseRobot.GUI;
 public class GUITest {
+    public static void main(String []arvgs){
+        GUI g=new GUI();
+
+    }
 }
