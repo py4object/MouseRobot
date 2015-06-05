bc08fffd18e07feb433ebd784cfd88b16bcd0ad6
Omar Zahed <py4object@gmail.com>
2015-06-05 12:17:17 +0300

    Added project launcher

--- src/main/java/com/github/mouseRobot/Main.java
+++ src/main/java/com/github/mouseRobot/Main.java
@@ -1,7 +1,21 @@
 package com.github.mouseRobot;
 
+import org.jnativehook.NativeHookException;
+
+import java.awt.*;
+
 /**
- * Created by kozo on 5/31/15.
+ * Created by Omar Zahed on 5/31/15.
  */
 public class Main {
+    public static void main(String []arvgs ) {
+        new GUI();
+        try {
+            MouseRobot d = new MouseRobot();
+        } catch (NativeHookException e) {
+            e.printStackTrace();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
 }
